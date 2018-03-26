7eaa3470c46cf5fac22b9362d093d77fbb6dfd19
Awsum4sum <jonnobud@gmail.com>
2018-03-26 21:59:25 +0100

    Add project files.

---
+++ Elevators/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Elevators.Models;
+
+namespace Elevators.Controllers
+{
+    public class HomeController : Controller
+    {
+        Random random = new Random();
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Elevator");
+        }
+
+        public IActionResult Elevator(ElevatorModel model)
+        {
+            model.ElevatorCount = 1;
+            model.FloorCount = random.Next(1, 100);
+            model.CurrentFloor = random.Next(model.FloorCount);
+            model.ElevatorList = new List<Elevator>();
+
+            for(int i = 0; i < 5; i++)
+            {
+                Elevator elevator = new Elevator();
+                int elevatorDirection = random.Next(2);
+
+                elevator.ElevatorCurrentFloor = random.Next(0, model.FloorCount);
+                elevator.ElevatorGoingUp = Convert.ToBoolean(elevatorDirection);
+                elevator.ElevatorID = i;
+
+                model.ElevatorList.Add(elevator);
+            }
+
+            return View(model);
+        }
+
+        public IActionResult ElevatorPartial(ElevatorModel model)
+        {
+            return PartialView("ElevatorPartial.cshtml", model);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Elevators/Models/ElevatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elevators.Models
+{
+    public class ElevatorModel
+    {
+        public int ElevatorCount { get; set; }
+        public int FloorCount { get; set; }
+        public int CurrentFloor { get; set; }
+        public int MaxFloors { get; set; }
+        public List<Elevator> ElevatorList { get; set; }
+    }
+
+    public class Elevator
+    {
+        public int ElevatorID { get; set; }
+        public int ElevatorCurrentFloor { get; set; }
+        public bool ElevatorGoingUp { get; set; }
+    }
+}

b028863d4e871bb231f2a7575e31a09ef7fe2a2e
Jonathan <jonnobud@gmail.com>
2018-03-29 14:56:25 +0100

    adding in movement of elevators

--- Elevators/Controllers/HomeController.cs
+++ Elevators/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -17,7 +17,7 @@ namespace Elevators.Controllers
             return RedirectToAction("Elevator");
         }
 
-        public IActionResult Elevator(ElevatorModel model)
+        public IActionResult Elevator(ElevatorModel model) //randomising locations and stats of 6 different elevators
         {
             model.ElevatorCount = 1;
             model.FloorCount = random.Next(5, 100);
@@ -34,6 +34,8 @@ namespace Elevators.Controllers
                 elevator.ElevatorID = i;
                 elevator.ElevatorDistance = Convert.ToUInt32(Math.Abs(model.CurrentFloor - elevator.ElevatorCurrentFloor));
                 elevator.ElevatorOpen = false;
+                elevator.ElevatorBottomFloor = 0;
+                elevator.ElevatorTopFloor = model.CurrentFloor;
                 if (elevator.ElevatorDistance == 0)
                     elevator.ElevatorOpen = true;
 
@@ -45,9 +47,61 @@ namespace Elevators.Controllers
 
         public IActionResult CallElevator(ElevatorModel model)
         {
+            List<Elevator> closestElevatorList = new List<Elevator>();
+            Elevator closestElevator = new Elevator();
 
+            closestElevatorList = model.ElevatorList.OrderBy(x => x.ElevatorDistance).ToList();
+
+            if (closestElevatorList.FirstOrDefault().ElevatorDistance == 0) //End if an elevator is already on the correct floor
+            {
+                return PartialView("ElevatorPartial.cshtml", model);
+            }
+
+            for (int j = 0; j < closestElevatorList.Count; j++) //Adding distance if elevator is not going in the correct direction
+            {
+                if (model.CallUp != closestElevatorList[j].ElevatorGoingUp)
+                {
+                    closestElevatorList[j].ElevatorDistance = closestElevatorList[j].ElevatorDistance + 3;
+                }
+            }
+
+            closestElevator = closestElevatorList.OrderBy(x => x.ElevatorDistance).Where(x => x.ElevatorFull.Equals(false)).FirstOrDefault(); //Sorting list again according to updated distances
+
+            for (int i = 0; i < model.ElevatorList.Count; i++)
+            {
+                //Getting total distance all elevators travel, assuming they all travel at the same speed and continue in the direction they were going
+                if (!model.ElevatorList[i].ElevatorBorked)
+                    model.ElevatorList[i] = ElevatorTravel(model.ElevatorList[i], closestElevator.ElevatorDistance);
+            }
 
             return PartialView("ElevatorPartial.cshtml", model);
+        }
+
+        public Elevator ElevatorTravel(Elevator elev, uint travelDistance)
+        {
+            for (int i = 0; i < travelDistance; i++)
+            {
+                if (elev.ElevatorGoingUp)
+                {
+                    if (elev.ElevatorTopFloor == elev.ElevatorCurrentFloor + 1) //if elevator hits the highest it can go, it changes direction
+                    {
+                        elev.ElevatorGoingUp = false;
+                    }
+
+                    elev.ElevatorCurrentFloor++;
+                }
+                if (!elev.ElevatorGoingUp)
+                {
+                    if (elev.ElevatorBottomFloor == elev.ElevatorCurrentFloor - 1)//if elevator hits the lowest it can go, it changes direction
+                    {
+                        elev.ElevatorGoingUp = true;
+                    }
+
+                    elev.ElevatorCurrentFloor--;
+                }
+            }
+
+            return elev;
         }
 
         public IActionResult Error()
@@ -56,3 +110,4 @@ namespace Elevators.Controllers
         }
     }
 }
+
